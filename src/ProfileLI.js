a8a2648693f6a4bf161bf15588365eda7abdb674
Scott <scrosehart@gmail.com>
2018-09-17 16:40:46 -0700

    more basic template set up

--- src/ProfileLI.js
+++ src/ProfileLI.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
-class Nav extends Component {
+class ProfileLI extends Component {
   constructor(props) {
     super(props)
   };
 
   render() {
     return (
-      <li>
-        <span>foo</span>
+      <li className='profile-li'>
+        <img src='imgs/BW_20180301_GA_Scott_Rosehart_24104.jpg' />
+        <span>Scott</span>
       </li>
     )
   }
 };
 
-export default Nav;
+export default ProfileLI;
